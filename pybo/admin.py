5f62db4020ed0c7537bab7b01528e2711d8af696
Dongjewerly <boo417@hanyang.ac.kr>
2025-04-14 11:55:43 +0900

    6weekslecture_createsuperuser

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Question
+
+class QuestionAdmin (admin.ModelAdmin):  #QuestionAdmin 클래스 생성(admin 모듈의 ModelAdmin 속성 가져옴)
+    search_fields = ['subject'] #검색 필드 생성, subject로 검색
+
+admin.site.register(Question, QuestionAdmin) #admin.site(admin화면)에 QuestionAdmin 클래스 추가
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by("-create_date") #question_list (변수) 생성, 오름차순으로 create_Date 작성 일시로 정리해라
+    context = {'question_list':question_list} #context 변수의 question_list (속성)으로 지정, 생성
+    return render(request,'pybo/question_list.html',context)
+    #question_list를 pybo/question_list.html 템플릿을 적용하여 html 형태로 변환 후 반환
+    
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
 # Create your views here.
