225b6d68d5f648eb9053482d376536729cd563fb
Dongjewerly <boo417@hanyang.ac.kr>
2025-05-12 11:38:24 +0900

    8weekslecture_answermodel

--- pybo/urls.py
+++ pybo/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'pybo'
+
 urlpatterns = [
-    path('',views.index)
-]
+    path('',views.index, name = 'index'),
+    path('<int:question_id>/', views.detail, name = 'detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+] #id로 만든 url과 질문 상세 화면을 연결, 정수형:질문id/ 된 url을 views파일의 detail함수와 연결
--- pybo/views.py
+++ pybo/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 def index(request):
-    question_list = Question.objects.order_by("-create_date") #question_list (변수) 생성, 오름차순으로 create_Date 작성 일시로 정리해라
+    question_list = Question.objects.order_by("-create_date") # question_list (변수) 생성, 오름차순으로 create_Date 작성 일시로 정리해라
     context = {'question_list':question_list} #context 변수의 question_list (속성)으로 지정, 생성
     return render(request,'pybo/question_list.html',context)
     #question_list를 pybo/question_list.html 템플릿을 적용하여 html 형태로 변환 후 반환
-    
+
+def detail(request, question_id):
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question':question}
+    return render(request,'pybo/question_detail.html',context)
     # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    question.answer_set.create(content=request.POST.get('content'),  create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question_id)
 # Create your views here.
+
