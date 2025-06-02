1c8885686f1bdee404000243333079032b26fe2d
Dongjewerly <boo417@hanyang.ac.kr>
2025-06-02 11:21:20 +0900

    11weekslecture_navbar만들기,page넘기기

--- pybo/urls.py
+++ pybo/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('',views.index, name = 'index'),
     path('<int:question_id>/', views.detail, name = 'detail'),
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('question/create/', views.question_create, name='question_create'),
 ] #id로 만든 url과 질문 상세 화면을 연결, 정수형:질문id/ 된 url을 views파일의 detail함수와 연결
--- pybo/views.py
+++ pybo/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Question
+from .models import Question, Answer
+from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
+from django.http import HttpResponseNotAllowed
+from django.core.paginator import Paginator
 
 def index(request):
+    page =   request.GET.get('page', '1')
     question_list = Question.objects.order_by("-create_date") # question_list (변수) 생성, 오름차순으로 create_Date 작성 일시로 정리해라
-    context = {'question_list':question_list} #context 변수의 question_list (속성)으로 지정, 생성
+    paginator = Paginator(question_list,10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    # context = {'question_list':question_list} #context 변수의 question_list (속성)으로 지정, 생성
+
     return render(request,'pybo/question_list.html',context)
     #question_list를 pybo/question_list.html 템플릿을 적용하여 html 형태로 변환 후 반환
 
@@ -21,5 +30,41 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     question.answer_set.create(content=request.POST.get('content'),  create_date=timezone.now())
     return redirect('pybo:detail', question_id=question_id)
-# Create your views here.
+
+
+def question_create(request):
+    if request.method == 'POST':  # 요청이 POST방식이면
+        form = QuestionForm(request.POST)  # POST 내용으로 QuestionForm()을 호출하여 form을 만듬
+        if form.is_valid():  # 폼이 유효하다면, 아래 설명보기
+            question = form.save(commit=False)  # 임시 저장하여 question 객체를 리턴
+            question.create_date = timezone.now()  # 실제 저장을 위해 작성일시를 설정
+            question.save()  # 데이터를 실제로 저장
+            return redirect('pybo:index')  # pybo:index로 이동함, 실주소는 …/pybo/
+    else:  # 요청이 GET방식이면(POST방식이 아니면)
+        form = QuestionForm()  # 인수 없이 QuestionForm()을 호출, GET 방식
+
+    context = {'form': form}  # 위에서 선택된 form이 context의 ‘form’이 됨
+    return render(request, 'pybo/question_form.html', context)
+    # context의 form을 question_form.html템플릿으로 변형
+    # 결국 선택된 QuestionForm을 question_form.html템플릿으로 변형
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":  # POST 방식이라면,
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:  # GET 방식이면(POST 방식이 아니라면)
+        return HttpResponseNotAllowed('Only POST is possible.')  # ‘POST만 가능합니다' 오류 발생
+
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
